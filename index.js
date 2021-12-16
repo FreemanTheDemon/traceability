a402f51ac8bf30f8b243b0421e33fea7e6a1c986
Freeman Bird <freemanbird@gmail.com>
2021-12-16 14:16:47 -0700

    added rollbar

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '4bbd93d53f494be7b5f12a95ef502452',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
+    rollbar.info('HTML file served')
 });
 
 const port = process.env.PORT || 4545;
